720864c96afacc6b0465db1a103dca50395d1247
shenhailuanma <shenhailuanma0512@163.com>
2015-05-15 15:26:34 +0800

    modify the create_quild.sql for format error.

--- sql/create_guild.sql
+++ sql/create_guild.sql
@@ -1,7 +1,7 @@
 
 
 DROP TABLE IF EXISTS `guild2`;
-CREATE TABLE `guild` (
+CREATE TABLE `guild2` (
     `id` int NOT NULL AUTO_INCREMENT COMMENT 'the id of the guild',
     `name` varchar(32) NOT NULL DEFAULT '' COMMENT 'the name of the guild',
     `head` varchar(128) NOT NULL DEFAULT '' COMMENT 'the head path of the guild',
@@ -16,15 +16,15 @@ CREATE TABLE `guild` (
     `latitude`  float NOT NULL DEFAULT 0 COMMENT 'the latitude of the guild',
     `limit` int NOT NULL DEFAULT 0 COMMENT 'the member limit of the guild',
     `number` int NOT NULL DEFAULT 0 COMMENT 'the member numbers of the guild',
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `guildMember2`;
-CREATE TABLE `guildMember` (
+CREATE TABLE `guildMember2` (
     `id` int NOT NULL AUTO_INCREMENT,
     `guildID` int NOT NULL DEFAULT 0 ,
     `playerID` int NOT NULL DEFAULT 0 ,
     `playerOpenID` int NOT NULL DEFAULT 0 ,
     `exp` int NOT NULL DEFAULT 0 ,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
